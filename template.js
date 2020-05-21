fd8bf78a105c0315c364d07272a6ad1f2e38c0d1
lulegend89 <60370755+lulegend89@users.noreply.github.com>
2020-05-21 12:46:48 +0200

    multiple dropdown add function + style

--- template.js
+++ template.js
@@ -21,6 +21,15 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
