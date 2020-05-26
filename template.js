5e80275b53710c6984a7d49f17acb419925ae19a
lulegend89 <60370755+lulegend89@users.noreply.github.com>
2020-05-26 17:01:57 +0200

    responsive opdatering

--- template.js
+++ template.js
@@ -29,6 +29,20 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+var acc = document.getElementsByClassName("accordion3");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
 
 
 /* When the user clicks on the button,
