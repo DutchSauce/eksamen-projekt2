ecc523092dd0721c185c15dfd91908cbfaaec6b5
emmanielsen <emma6007@edu.ucl.dk>
2020-06-04 12:44:11 +0200

    forbedringer

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
 console.log("JS is running");
-/*--------- Kørende baggrund ----------*/
-var bg = new Image();
-bg.src = "img/baggrund.png";
-function initCanvas(){
-    var ctx = document.getElementById('my_canvas').getContext('2d');
-    var cW = ctx.canvas.width, cH = ctx.canvas.height;
-
-    function Background(){
-        this.x = 0, this.y = 0, this.w = bg.width, this.h = bg.height;
-        this.render = function(){
-            ctx.drawImage(bg, this.x--, 0);
-            if(this.x <= -2000){
-                this.x = 0;
-            }
-        }
-    }
-    var background = new Background();
-
-    function animate(){
-        ctx.save();
-        ctx.clearRect(0, 0, cW, cH);
-        background.render();
-        ctx.restore();
-    }
-    var animateInterval = setInterval(animate, 30);
-}
-window.addEventListener('load', function(event) {
-    initCanvas();
-});
-   
 /* ------ menu baren burger menu -------*/
 function myfunction(menubar) {
     let x = document.getElementById("myLinks");
